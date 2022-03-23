5e2b2d6a9e572bc920dcf2b032b489615067cde8
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-03-23 12:57:45 -0600

    completed coding challenge about objects, methods and the this keyword

--- 02-Fundamentals-Part-2/final/script.js
+++ 02-Fundamentals-Part-2/final/script.js
@@ -385,40 +385,37 @@ TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.9
 GOOD LUCK 😀
 */
 
-/*
 const mark = {
   fullName: 'Mark Miller',
   mass: 78,
   height: 1.69,
-  calcBMI: function () {
+
+  calcBmi() {
     this.bmi = this.mass / this.height ** 2;
+
     return this.bmi;
-  }
+  },
 };
 
 const john = {
   fullName: 'John Smith',
   mass: 92,
   height: 1.95,
-  calcBMI: function () {
+
+  calcBmi() {
     this.bmi = this.mass / this.height ** 2;
+
     return this.bmi;
-  }
+  },
 };
 
-mark.calcBMI();
-john.calcBMI();
+mark.calcBmi();
+john.calcBmi();
 
-console.log(mark.bmi, john.bmi);
+console.log(mark.bmi);
+console.log(john.bmi);
 
-// "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
-
-if (mark.bmi > john.bmi) {
-  console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s BMI (${john.bmi})`)
-} else if (john.bmi > mark.bmi) {
-  console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s BMI (${mark.bmi})`)
-}
-
+/*
 
 ///////////////////////////////////////
 // Iteration: The for Loop
