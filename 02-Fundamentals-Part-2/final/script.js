7e57afef7c7f6f5f4ade3650093b658c7b26ad60
Kevin Castro <kevin.castro.chavez0@gmail.com>
2022-03-24 13:31:01 -0600

    completed coding challenge using arrow functions, reduce method and for loops

--- 02-Fundamentals-Part-2/final/script.js
+++ 02-Fundamentals-Part-2/final/script.js
@@ -385,35 +385,35 @@ TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.9
 GOOD LUCK 😀
 */
 
-const mark = {
-  fullName: 'Mark Miller',
-  mass: 78,
-  height: 1.69,
+// const mark = {
+//   fullName: 'Mark Miller',
+//   mass: 78,
+//   height: 1.69,
 
-  calcBmi() {
-    this.bmi = this.mass / this.height ** 2;
+//   calcBmi() {
+//     this.bmi = this.mass / this.height ** 2;
 
-    return this.bmi;
-  },
-};
+//     return this.bmi;
+//   },
+// };
 
-const john = {
-  fullName: 'John Smith',
-  mass: 92,
-  height: 1.95,
+// const john = {
+//   fullName: 'John Smith',
+//   mass: 92,
+//   height: 1.95,
 
-  calcBmi() {
-    this.bmi = this.mass / this.height ** 2;
+//   calcBmi() {
+//     this.bmi = this.mass / this.height ** 2;
 
-    return this.bmi;
-  },
-};
+//     return this.bmi;
+//   },
+// };
 
-mark.calcBmi();
-john.calcBmi();
+// mark.calcBmi();
+// john.calcBmi();
 
-console.log(mark.bmi);
-console.log(john.bmi);
+// console.log(mark.bmi);
+// console.log(john.bmi);
 
 /*
 
@@ -560,30 +560,27 @@ HINT: Call calcTip in the loop and use the push method to add values to the tips
 GOOD LUCK 😀
 */
 
-/*
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-}
 const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
+let tips = [];
+let totals = [];
+
+const calcTip = (bill) => {
+  const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+  return tip;
+};
 
 for (let i = 0; i < bills.length; i++) {
-  const tip = calcTip(bills[i]);
-  tips.push(tip);
-  totals.push(tip + bills[i]);
+  tips.push(calcTip(bills[i]));
 }
-console.log(bills, tips, totals);
 
-const calcAverage = function (arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    // sum = sum + arr[i];
-    sum += arr[i];
-  }
-  return sum / arr.length;
+for (let i = 0; i < bills.length; i++) {
+  totals.push(bills[i] + tips[i]);
 }
-console.log(calcAverage([2, 3, 7]));
-console.log(calcAverage(totals));
-console.log(calcAverage(tips));
-*/
+
+console.log(tips);
+console.log(totals);
+
+// const tips = bills.map((bill) => calcTip(bill));
+// const totals = bills.map((bill, index) => bill + tips[index]);
+// console.log(totals);
